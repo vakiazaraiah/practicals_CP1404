a2800ed55b35b59b9fb1caf8bfb6d780d95c1ab7
vakiazaraiah <azaraiah.vaki@my.jcu.edu.au>
2018-08-22 13:52:28 +1000

    Add password_checker

---
+++ prac_04/username.py
@@ -0,0 +1,10 @@
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username_entered = input("Please enter username: ")
+if username_entered in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied.")

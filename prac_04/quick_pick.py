6f67a86ea090a257410b5ed84198e4e019fff371
vakiazaraiah <azaraiah.vaki@my.jcu.edu.au>
2018-08-23 10:49:52 +1000

    finish quick picks

---
+++ prac_04/quick_pick.py
@@ -0,0 +1,30 @@
+
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+
+def main():
+    """Quick picks program - choose sets of random numbers."""
+    number_of_quick_picks = int(input("Quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Please try again!")
+        number_of_quick_picks = int(input("Quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_picks = []
+        for l in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_picks:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_picks.append(number)
+        quick_picks.sort()
+        # the following uses a generator expression (like a list comprehension)
+        # to format each number in quick_picks in the same way
+        # this is then turned into a single string with the join method
+        print(" ".join("{:3}".format(number) for number in quick_picks))
+
+
+main()

015d068df7ce2f3a8de697482964ef1a9216b5a2
vakiazaraiah <azaraiah.vaki@my.jcu.edu.au>
2018-09-10 12:51:01 +1000

    Add states program to include lower caps as well.

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,21 +5,14 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-               "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+STATE_NAMES = {"QLD" and "qld": "Queensland", "NSW" and "nsw": "New South Wales", "NT" and "nt": "Northern Territory", "WA" and "wa": "Western Australia",
+               "ACT" and "act": "Australian Capital Territory", "VIC" and "vic": "Victoria", "TAS" and "tas": "Tasmania"}
+# print(STATE_NAMES)
 
-print(STATE_NAMES)
-
-state = input("Enter short state: ").upper()
+state = input("Enter short state: ")
 while state != "":
     if state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
-
-for state in STATE_NAMES.keys():
-    full_name = STATE_NAMES[state]
-    print(state, "is", full_name)
-
-
+    state = input("Enter short state: ")
